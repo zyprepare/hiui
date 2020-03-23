6bc042e267666d28edeab482a6c7d25cfb7b9770
Wugaoliang <wugaoliang@xiaomi.com>
2020-03-23 11:18:46 +0800

    feat: search

--- components/search/index.tsx
+++ components/search/index.tsx
@@ -1,94 +1,100 @@
-import React, {useState,useRef} from 'react'
+
+import React, { useState, useRef } from 'react'
 import Input from '../input'
 import Button from '../button'
 import SearchDropdown from './searchDropdown'
-import {DataItem,SearchProps} from './types'
+import { DataItem, SearchProps } from './types'
 import Provider from '../context'
 
 import './style'
 
-const Search: React.FC<SearchProps> = props=> {
-    const [dropdownShow, setDropdownShow] = useState(false)
-    const searchInputContainer  = useRef(null)
-    const [inputVal,setInputVal] = useState('')
-    const [enterDropdown,setEnterDropdown] = useState(false)
-    const { 
-        onChange,
-        onSearch,
-        style,
-        placeholder,
-        prepend,
-        disabled,
-        historyData,
-        data,
-        onDelete,
-        localeDatas
-    } = props
-    const closeDropdown = (e) => {
-        setDropdownShow(e.target.className === 'hi-input__text ') 
-    }
-    
-    const itemClick = (value,item: DataItem) => {
-        setInputVal(value)
-        setDropdownShow(false)
-        onSearch && onSearch(value,item)
-    } 
-    const prefixCls = 'hi-search'
-    
-    return (
-        <div className = {prefixCls} style={style}>
-            <div className = {`${prefixCls}_input`} ref={searchInputContainer}>
-                <Input
-                    type="text"
-                    value={inputVal}
-                    style ={style}
-                    disabled = {disabled}
-                    placeholder= { placeholder }
-                    clearable="true"
-                    prepend = {prepend}
-                    onFocus = {(e)=>{
-                        historyData && closeDropdown(e)
-                    }}
-                    onBlur = {()=>{
-                        setDropdownShow(enterDropdown)
-                    }}
-                    onChange = {(e)=>{
-                        const {value} = e.target
-                        setInputVal(value)
-                        data && value.length>0 && setDropdownShow(true)
-                        onChange && onChange(value)
-                    }}
-                />
-                <Button type="default" icon='search' disabled={disabled} onClick={(e) => {
-                    e.preventDefault()
-                    closeDropdown(e)
-                    if(!inputVal){
-                        return
-                    }
-                    onSearch && onSearch(inputVal)
-                }} />
-            </div>
-            { data || historyData ? <SearchDropdown  
-                    prefixCls = {prefixCls} 
-                    inputVal = {inputVal}
-                    itemClick = {itemClick}
-                    data = {data}
-                    onDelete = {onDelete}
-                    dropdownShow = {dropdownShow}
-                    localeDatas = {localeDatas}
-                    onMouseEnter = {()=>{
-                        setEnterDropdown(true)
-                    }}
-                    onMouseLeave = {()=>{
-                        setEnterDropdown(false)
-                    }}
-                    searchInputContainer = {searchInputContainer}
-                    historyData = {historyData}
-                /> : null
-            }
-        </div>
-    )
-}
+const Search: React.FC<SearchProps> = props => {
+  const [dropdownShow, setDropdownShow] = useState(false)
+  const searchInputContainer = useRef(null)
+  const [inputVal, setInputVal] = useState('')
+  const [enterDropdown, setEnterDropdown] = useState(false)
+  const {
+    onChange,
+    onSearch,
+    style,
+    placeholder,
+    prepend,
+    disabled,
+    historyData,
+    data,
+    onDelete,
+    localeDatas
+  } = props
+  const closeDropdown = e => {
+    setDropdownShow(e.target.className === 'hi-input__text ')
+  }
 
+  const itemClick = (value, item: DataItem) => {
+    setInputVal(value)
+    setDropdownShow(false)
+    onSearch && onSearch(value, item)
+  }
+  const prefixCls = 'hi-search'
+
+  return (
+    <div className={prefixCls} style={style}>
+      <div className={`${prefixCls}_input`} ref={searchInputContainer}>
+        <Input
+          type='text'
+          value={inputVal}
+          style={style}
+          disabled={disabled}
+          placeholder={placeholder}
+          clearable='true'
+          prepend={prepend}
+          onFocus={e => {
+            historyData && closeDropdown(e)
+          }}
+          onBlur={() => {
+            setDropdownShow(enterDropdown)
+          }}
+          onChange={e => {
+            const { value } = e.target
+            setInputVal(value)
+            data && value.length > 0 && setDropdownShow(true)
+            onChange && onChange(value)
+          }}
+        />
+        <Button
+          type='default'
+          icon='search'
+          disabled={disabled}
+          onClick={e => {
+            e.preventDefault()
+            closeDropdown(e)
+            if (!inputVal) {
+              return
+            }
+            onSearch && onSearch(inputVal)
+          }}
+        />
+      </div>
+      {data || historyData ? (
+        <SearchDropdown
+          prefixCls={prefixCls}
+          inputVal={inputVal}
+          itemClick={itemClick}
+          data={data}
+          onDelete={onDelete}
+          dropdownShow={dropdownShow}
+          localeDatas={localeDatas}
+          onMouseEnter={() => {
+            setEnterDropdown(true)
+          }}
+          onMouseLeave={() => {
+            setEnterDropdown(false)
+          }}
+          searchInputContainer={searchInputContainer}
+          historyData={historyData}
+        />
+      ) : null}
+    </div>
+  )
+}
 
 export default Provider(Search)
--- components/search/searchDropdown.tsx
+++ components/search/searchDropdown.tsx
@@ -1,141 +1,173 @@
+
 import React from 'react'
 import classNames from 'classnames'
 import Icon from '../icon'
 import Popper from '../popper'
-import {DataItem,SearchDropdownPorps} from './types'
+import { DataItem, SearchDropdownPorps } from './types'
 
-const SearchDropdown :React.FC<SearchDropdownPorps> = props => {
-    const {
-        data,
-        prefixCls,
-        itemClick,
-        historyData,
-        inputVal = '',
-        onDelete,
-        dropdownShow,
-        searchInputContainer,
-        onMouseLeave,
-        onMouseEnter,
-        localeDatas
-    } = props
-    const hightlightKeyword = (title, uniqueKey):React.ReactNode => {
-        const searchbarValue = inputVal
-        let keyword = inputVal
-        keyword = searchbarValue.includes('[') ? keyword.replace(/\[/gi, '\\[') : keyword
-        keyword = searchbarValue.includes('(') ? keyword.replace(/\(/gi, '\\(') : keyword
-        keyword = searchbarValue.includes(')') ? keyword.replace(/\)/gi, '\\)') : keyword
-    
-        const parts = title.split(new RegExp(`(${keyword})`, 'gi'))
-        return (
-            inputVal && inputVal.length > 0 ? <p key={uniqueKey}>
-            { parts.map((part) =>
-              part === searchbarValue
-                ? <span key={uniqueKey} className='hi-search_dropdown--item__name-hightlight'>
-                   { part }
-                </span>
-                : part
-            )
-            }
-          </p>
-            : title
-        )
-    }
-    const ItemChildren = (item: DataItem):React.ReactNode => {
-        return (
-            <ul>{
-                item.children && item.children.map(ele=>{
-                    return <li className={`${prefixCls}_dropdown--item`} style={{padding: 0}} key={ele.id} >
-                        <span 
-                            className={`${prefixCls}_dropdown--item_normal`}
-                            onClick={() => {
-                                itemClick(typeof ele.title === 'string' ? ele.title : ele.id,ele)
-                            }}
-                        >
-                            {typeof ele.title === 'string' ? hightlightKeyword(ele.title,ele.id) : ele.title}
-                        </span>
-                    </li>
-                })
-            }
-            </ul>
-        )
-    }
-    
-    const DataSourceRender = (item: DataItem):React.ReactNode=> {
-        const className = classNames(
-            `${prefixCls}_dropdown--item_normal`,
-            {[`${prefixCls}_dropdown--item-title`] :item.children}
-        )
-        return (
-            <li 
-                className={`${prefixCls}_dropdown--item`} 
-                key={item.id} 
+const SearchDropdown: React.FC<SearchDropdownPorps> = props => {
+  const {
+    data,
+    prefixCls,
+    itemClick,
+    historyData,
+    inputVal = '',
+    onDelete,
+    dropdownShow,
+    searchInputContainer,
+    onMouseLeave,
+    onMouseEnter,
+    localeDatas
+  } = props
+  const hightlightKeyword = (title, uniqueKey): React.ReactNode => {
+    const searchbarValue = inputVal
+    let keyword = inputVal
+    keyword = searchbarValue.includes('[')
+      ? keyword.replace(/\[/gi, '\\[')
+      : keyword
+    keyword = searchbarValue.includes('(')
+      ? keyword.replace(/\(/gi, '\\(')
+      : keyword
+    keyword = searchbarValue.includes(')')
+      ? keyword.replace(/\)/gi, '\\)')
+      : keyword
+
+    const parts = title.split(new RegExp(`(${keyword})`, 'gi'))
+    return inputVal && inputVal.length > 0 ? (
+      <p key={uniqueKey}>
+        {parts.map(part =>
+          part === searchbarValue ? (
+            <span
+              key={uniqueKey}
+              className='hi-search_dropdown--item__name-hightlight'
             >
-                <span 
-                    className={className} 
-                    onClick={() => {
-                        itemClick(typeof item.title === 'string' ? item.title : item.id,item)
-                    }}
-                >
-                    {typeof item.title === 'string' ? hightlightKeyword(item.title,item.id) : item.title}
-                </span>
-                {
-                    item.children && ItemChildren(item)
-                } 
-            </li>
-        )
-    }
-    
-    const HistoryRender = ():React.ReactNode => {
-        const {searchRecord, searchEmptyRecord} = localeDatas.search
-        const HistoryTitle = inputVal.length === 0 && historyData && historyData.length > 0 ? 
-                            <li className={`${prefixCls}_dropdown--item ${prefixCls}_dropdown--item-history`}>
-                                <span>{searchRecord}</span>
-                                <Icon name='delete' onClick={()=>{
-
-                                    onDelete && onDelete()
-                                }}/>
-                            </li> : null
-        const HistoryNoData = <li className={`${prefixCls}_dropdown--item-nodata`}> {searchEmptyRecord} </li>
-        const showHistoryNode = inputVal.length === 0 && historyData && historyData.length === 0
-        return (
-            showHistoryNode ? HistoryNoData : HistoryTitle
-
-        )
-    }
-    
-    const dataRender = inputVal.length ? data : historyData
-    const {searchEmptyResult} = localeDatas.search
-    return (
-        <Popper
-            show={dropdownShow}
-            attachEle={searchInputContainer.current}
-            zIndex={1050}
-            topGap={5}
-            onMouseEnter = {()=>{
-                onMouseEnter()
-            }}
-            onMouseLeave = {()=>{
-                onMouseLeave()
-            }}
-            className={`${prefixCls}__popper`}
-            placement="top-bottom-start">
-            <div className ={`${prefixCls}_dropdown`}>
-                    <ul className ={`${prefixCls}_dropdown--items`}>
-                    { HistoryRender() }
-                    {
-                        dataRender && dataRender.map((item)=>{
-                            return DataSourceRender(item)
-                        })
-                    }
-                    {
-                        (!data || data.length === 0) && <li className={`${prefixCls}_dropdown--item-nodata`}> {searchEmptyResult} </li>
-                    }
-                </ul>
-            </div>
-        </Popper>
+              {part}
+            </span>
+          ) : (
+            part
+          )
+        )}
+      </p>
+    ) : (
+      title
     )
+  }
+  const ItemChildren = (item: DataItem): React.ReactNode => {
+    return (
+      <ul>
+        {item.children &&
+          item.children.map(ele => {
+            return (
+              <li
+                className={`${prefixCls}_dropdown--item`}
+                style={{ padding: 0 }}
+                key={ele.id}
+              >
+                <span
+                  className={`${prefixCls}_dropdown--item_normal`}
+                  onClick={() => {
+                    itemClick(
+                      typeof ele.title === 'string' ? ele.title : ele.id,
+                      ele
+                    )
+                  }}
+                >
+                  {typeof ele.title === 'string'
+                    ? hightlightKeyword(ele.title, ele.id)
+                    : ele.title}
+                </span>
+              </li>
+            )
+          })}
+      </ul>
+    )
+  }
+
+  const DataSourceRender = (item: DataItem): React.ReactNode => {
+    const className = classNames(`${prefixCls}_dropdown--item_normal`, {
+      [`${prefixCls}_dropdown--item-title`]: item.children
+    })
+    return (
+      <li className={`${prefixCls}_dropdown--item`} key={item.id}>
+        <span
+          className={className}
+          onClick={() => {
+            itemClick(
+              typeof item.title === 'string' ? item.title : item.id,
+              item
+            )
+          }}
+        >
+          {typeof item.title === 'string'
+            ? hightlightKeyword(item.title, item.id)
+            : item.title}
+        </span>
+        {item.children && ItemChildren(item)}
+      </li>
+    )
+  }
+
+  const HistoryRender = (): React.ReactNode => {
+    const { searchRecord, searchEmptyRecord } = localeDatas.search
+    const HistoryTitle =
+      inputVal.length === 0 && historyData && historyData.length > 0 ? (
+        <li
+          className={`${prefixCls}_dropdown--item ${prefixCls}_dropdown--item-history`}
+        >
+          <span>{searchRecord}</span>
+          <Icon
+            name='delete'
+            onClick={() => {
+              onDelete && onDelete()
+            }}
+          />
+        </li>
+      ) : null
+    const HistoryNoData = (
+      <li className={`${prefixCls}_dropdown--item-nodata`}>
+        {' '}
+        {searchEmptyRecord}{' '}
+      </li>
+    )
+    const showHistoryNode =
+      inputVal.length === 0 && historyData && historyData.length === 0
+    return showHistoryNode ? HistoryNoData : HistoryTitle
+  }
+
+  const dataRender = inputVal.length ? data : historyData
+  const { searchEmptyResult } = localeDatas.search
+  return (
+    <Popper
+      show={dropdownShow}
+      attachEle={searchInputContainer.current}
+      zIndex={1050}
+      topGap={5}
+      onMouseEnter={() => {
+        onMouseEnter()
+      }}
+      onMouseLeave={() => {
+        onMouseLeave()
+      }}
+      className={`${prefixCls}__popper`}
+      placement='top-bottom-start'
+    >
+      <div className={`${prefixCls}_dropdown`}>
+        <ul className={`${prefixCls}_dropdown--items`}>
+          {HistoryRender()}
+          {dataRender &&
+            dataRender.map(item => {
+              return DataSourceRender(item)
+            })}
+          {(!data || data.length === 0) && (
+            <li className={`${prefixCls}_dropdown--item-nodata`}>
+              {' '}
+              {searchEmptyResult}{' '}
+            </li>
+          )}
+        </ul>
+      </div>
+    </Popper>
+  )
 }
-
-
 
 export default SearchDropdown
