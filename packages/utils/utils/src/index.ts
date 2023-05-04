bd33848c28f4543b95961afab889f54c2c488f08
zhouyun1 <zhouyun1@xiaomi.com>
2023-05-04 20:34:56 +0800

    chore: test

--- packages/utils/utils/src/index.ts
+++ packages/utils/utils/src/index.ts
@@ -11,14 +11,14 @@ export * from '@hi-ui/type-assertion'
 export * from '@hi-ui/raf'
 export * from '@hi-ui/throttle-by-raf'
 
-function abc2() {
-  let arr2 = []
-  if (arr2.length === 0) {
-    arr2 = [2, 3, Math.random() * 10]
+function abc1() {
+  let arr1 = []
+  if (arr1.length === 0) {
+    arr1 = [2, 3, Math.random() * 10]
   }
 }
-console.log(a)
-b
+console.log(ab)
+bc
 function abc3() {
   let arr3 = []
   if (arr3.length < 0) {
